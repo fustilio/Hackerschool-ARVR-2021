938fabe7e483abd9044b759ee602bbc1799bc3ac
Glitch (hello-webpage) <none>
2021-04-09 17:13:02 +0000

    🏄🌼 Checkpoint ./vr_cubes_10.js:1160023/912

--- vr_cubes_10.js
+++ vr_cubes_10.js
@@ -54,20 +54,36 @@ function init() {
   );
   scene.add( room );
 
+  // Lets add a AxesHelper object to see the coordinate axes
+  // The X axis is red. The Y axis is green. The Z axis is blue.
+  const axesHelper = new THREE.AxesHelper( 1 );
+  axesHelper.position.x = 0;
+  axesHelper.position.y = 1
+  axesHelper.position.z = -2;
+  scene.add( axesHelper );
+  
   // prepare some geometry with which we will instantiate a cube
   const geometry = new THREE.BoxGeometry( 0.15, 0.15, 0.15 );
-  const myFirstCube = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial() );
-  myFirstCube.position.x = 0;
-  myFirstCube.position.y = 0;
-  myFirstCube.position.z = 0;
-
+  const myFirstCube = new THREE.Mesh( geometry, new THREE.MeshLambertMaterial( 0xff0000 ) );
+  // if you look at the scene now, you'll find that the cube is directly below you
+  
   room.add( myFirstCube );
   
-//   scene.add( new THREE.HemisphereLight( 0x606060, 0x404040 ) );
+  // lets move the cube to a better position, right above the axes helper
+  myFirstCube.position.x = 0;
+  myFirstCube.position.y = 2;
+  myFirstCube.position.z = -2;
 
-//   const light = new THREE.DirectionalLight( 0xffffff );
-//   light.position.set( 1, 1, 1 ).normalize();
-//   scene.add( light );
+  
+  // the cube is supposed to be red, what happened?
+
+
+  
+  scene.add( new THREE.HemisphereLight( 0x606060, 0x404040 ) );
+
+  const light = new THREE.DirectionalLight( 0xffffff );
+  light.position.set( 1, 1, 1 ).normalize();
+  scene.add( light );
 
 
 //   for ( let i = 0; i < 200; i ++ ) {
@@ -234,40 +250,40 @@ function render() {
 
 //   // Keep cubes inside room
 
-//   for ( let i = 0; i < room.children.length; i ++ ) {
+  for ( let i = 0; i < room.children.length; i ++ ) {
 
-//     const cube = room.children[ i ];
+    const cube = room.children[ i ];
 
-//     cube.userData.velocity.multiplyScalar( 1 - ( 0.001 * delta ) );
+    cube.userData.velocity.multiplyScalar( 1 - ( 0.001 * delta ) );
 
-//     cube.position.add( cube.userData.velocity );
+    cube.position.add( cube.userData.velocity );
 
-//     if ( cube.position.x < - 3 || cube.position.x > 3 ) {
+    if ( cube.position.x < - 3 || cube.position.x > 3 ) {
 
-//       cube.position.x = THREE.MathUtils.clamp( cube.position.x, - 3, 3 );
-//       cube.userData.velocity.x = - cube.userData.velocity.x;
+      cube.position.x = THREE.MathUtils.clamp( cube.position.x, - 3, 3 );
+      cube.userData.velocity.x = - cube.userData.velocity.x;
 
-//     }
+    }
 
-//     if ( cube.position.y < 0 || cube.position.y > 6 ) {
+    if ( cube.position.y < 0 || cube.position.y > 6 ) {
 
-//       cube.position.y = THREE.MathUtils.clamp( cube.position.y, 0, 6 );
-//       cube.userData.velocity.y = - cube.userData.velocity.y;
+      cube.position.y = THREE.MathUtils.clamp( cube.position.y, 0, 6 );
+      cube.userData.velocity.y = - cube.userData.velocity.y;
 
-//     }
+    }
 
-//     if ( cube.position.z < - 3 || cube.position.z > 3 ) {
+    if ( cube.position.z < - 3 || cube.position.z > 3 ) {
 
-//       cube.position.z = THREE.MathUtils.clamp( cube.position.z, - 3, 3 );
-//       cube.userData.velocity.z = - cube.userData.velocity.z;
+      cube.position.z = THREE.MathUtils.clamp( cube.position.z, - 3, 3 );
+      cube.userData.velocity.z = - cube.userData.velocity.z;
 
-//     }
+    }
 
-//     cube.rotation.x += cube.userData.velocity.x * 2 * delta;
-//     cube.rotation.y += cube.userData.velocity.y * 2 * delta;
-//     cube.rotation.z += cube.userData.velocity.z * 2 * delta;
+    cube.rotation.x += cube.userData.velocity.x * 2 * delta;
+    cube.rotation.y += cube.userData.velocity.y * 2 * delta;
+    cube.rotation.z += cube.userData.velocity.z * 2 * delta;
 
-//   }
+  }
 
   // Step 1: render the scene through the camera
   renderer.render( scene, camera );
